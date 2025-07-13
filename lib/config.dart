055aaaa2877c140fedd45286849890813b6f3e3f
Priyantha <kjpriyanthafdo@gmail.com>
2025-07-13 16:57:02 +0530

    zooming

--- lib/config.dart
+++ lib/config.dart
@@ -2,7 +2,10 @@ import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
 class Config {
-  static final backgroundColor = Colors.green;
+  static const backgroundColor = Colors.green;
   static const showGrid = true;
+  static const zoomSpeed = 0.002;
+  static const maxZoom = 5.0;
+
   static final worldSize = Vector2(1000, 1000);
 }
--- lib/game/bits_game.dart
+++ lib/game/bits_game.dart
@@ -1,13 +1,18 @@
+import 'dart:math' as math;
+
 import 'package:bits/config.dart';
 import 'package:bits/game/bits_world.dart';
 import 'package:bits/utils/extensions/color_extension.dart';
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-class BitsGame extends FlameGame {
+class BitsGame extends FlameGame with ScrollDetector {
   late final BitsWorld bitsWorld;
+  late final Vector2 worldSize;
   late final CameraComponent cameraComponent;
+  late final double _minZoom;
 
   @override
   Color backgroundColor() {
@@ -17,13 +22,9 @@ class BitsGame extends FlameGame {
   @override
   Future<void> onLoad() async {
     bitsWorld = BitsWorld();
+    worldSize = Config.worldSize;
 
-    cameraComponent = CameraComponent.withFixedResolution(
-      world: bitsWorld,
-      width: Config.worldSize.x + 50,
-      height: Config.worldSize.y + 50,
-    );
-
+    cameraComponent = CameraComponent(world: bitsWorld);
     cameraComponent.viewfinder.anchor = Anchor.center;
     cameraComponent.viewfinder.position = Vector2.zero();
 
@@ -31,5 +32,25 @@ class BitsGame extends FlameGame {
       cameraComponent,
       bitsWorld,
     ]);
+
+    final screenSize = size;
+    final worldSizeWithMargin = worldSize * 1.05;
+
+    _minZoom = math.min(
+      screenSize.x / worldSizeWithMargin.x,
+      screenSize.y / worldSizeWithMargin.y,
+    );
+
+    cameraComponent.viewfinder.zoom = _minZoom;
+  }
+
+  @override
+  void onScroll(PointerScrollInfo info) {
+    const zoomSpeed = Config.zoomSpeed;
+
+    final deltaZoom = -info.scrollDelta.global.y * zoomSpeed;
+    final newZoom = (cameraComponent.viewfinder.zoom + deltaZoom).clamp(_minZoom, Config.maxZoom);
+
+    cameraComponent.viewfinder.zoom = newZoom;
   }
 }
